93e673d4492ad8753d53343fd0a2f5f5b43a4979
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-03-03 09:22:13 -0300

    botões de ação adicionados na tarefa.

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,12 +1,50 @@
 import styles from "./Task.module.css";
+import Image from "next/image";
+import CalendarIcon from "/public/assets/icons/calendarIcon.svg";
+import EditTaskIcon from "/public/assets/icons/editTaskIcon.svg";
+import DeleteTaskIcon from "/public/assets/icons/deleteTaskIcon.svg";
+import { useState } from "react";
 
 export default function Task() {
+  const [showExtraActions, setShowExtraIcons] = useState<boolean>(false);
+
   return (
-    <div className={styles.taskContainer}>
+    <div
+      className={styles.taskContainer}
+      onMouseEnter={() => setShowExtraIcons(true)}
+      onMouseLeave={() => setShowExtraIcons(false)}
+    >
       <div className={styles.leftItems}>
         <div className={styles.taskCheck}></div> nome
       </div>
-      <div>due date</div>
+      <div className={styles.rightItems}>
+        {showExtraActions && (
+          <>
+            <Image
+              src={EditTaskIcon}
+              width={30}
+              height={30}
+              alt="ícone de edição"
+              className={styles.actionableWhiteIcon}
+            />
+            <Image
+              src={DeleteTaskIcon}
+              width={30}
+              height={30}
+              alt="Ícone de exclusão"
+              className={styles.actionableWhiteIcon}
+            />
+          </>
+        )}
+        <Image
+          src={CalendarIcon}
+          width={30}
+          height={30}
+          alt="Ícone de calendário"
+          className={styles.whiteIcon}
+        />{" "}
+        12 Jul. 2024
+      </div>
     </div>
   );
 }
