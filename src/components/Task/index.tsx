a84a32754a084cde0be4020eed1c1e2d1bdbfae3
gentil-kadson <fernandes.gentilkadsonjac@gmail.com>
2024-03-03 08:58:02 -0300

    task component created.

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -1,5 +1,12 @@
 import styles from "./Task.module.css";
 
 export default function Task() {
-  return <div>Task here</div>;
+  return (
+    <div className={styles.taskContainer}>
+      <div className={styles.leftItems}>
+        <div className={styles.taskCheck}></div> nome
+      </div>
+      <div>due date</div>
+    </div>
+  );
 }
